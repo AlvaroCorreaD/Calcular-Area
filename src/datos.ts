7296a1ae5cadeafe18dcbd08ed5b8190bddd6c15
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-04-28 22:08:30 -0300

    wip

--- src/datos.ts
+++ src/datos.ts
@@ -1,8 +1,19 @@
-let btnEnv = document.getElementById("btnEnviar");
-let datos = document.getElementById("datos");
-let rotulo = document.getElementById("rotulo");
-rotulo.innerHTML = "Ingrese un dato";
-// btnEnviar.innerHTML = "Esto es un boton";
+// let btnEnv = document.getElementById("btnEnviar");
+// let datos = document.getElementById("datos");
+// let rotulo = document.getElementById("rotulo");
+// rotulo.innerHTML = "Ingrese un dato";
+// // btnEnviar.innerHTML = "Esto es un boton";
+// btnEnv.addEventListener("click", () => {
+//   console.log("el dato ingresado es ", datos.value);
+// });
+
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+let base: number = Number(dato1.value);
+let altura: number = Number(dato2.value);
+let area: number = base * altura;
+let btnEnv = document.getElementById("btnEnviarbase");
+let btnEnv = document.getElementById("btnEnviaraltura");
 btnEnv.addEventListener("click", () => {
-  console.log("el dato ingresado es ", datos.value);
+  console.log("El area es:", area);
 });

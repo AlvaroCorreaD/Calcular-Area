6631146342fa6eb70d1d5c7c5cfe13bf48fab4bb
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-04-28 21:18:34 -0300

    Ejercicio-28-4

--- src/datos.ts
+++ src/datos.ts
@@ -1,10 +1,7 @@
 let btnEnv = document.getElementById("btnEnviar");
-let dato = document.getElementById("dato");
+let datos = document.getElementById("datos");
 let rotulo = document.getElementById("rotulo");
+rotulo.innerHTML = "Ingrese un dato";
 btnEnv.addEventListener("click", () => {
-  // TU CODIGO VA AQUI, POR EJEMPLO:
-  console.log("el dato ingresado es ", dato.value);
-  rotulo.innerHTML = "Ingrese un dato";
-
-  //}
+  console.log("el dato ingresado es ", datos.value);
 });

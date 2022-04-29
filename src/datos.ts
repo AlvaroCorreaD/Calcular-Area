dc5f78810d7ca0921fdcc970098a10171ccea1cf
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-04-28 21:28:20 -0300

    commit btn

--- src/datos.ts
+++ src/datos.ts
@@ -2,6 +2,7 @@ let btnEnv = document.getElementById("btnEnviar");
 let datos = document.getElementById("datos");
 let rotulo = document.getElementById("rotulo");
 rotulo.innerHTML = "Ingrese un dato";
+// btnEnviar.innerHTML = "Esto es un boton";
 btnEnv.addEventListener("click", () => {
   console.log("el dato ingresado es ", datos.value);
 });

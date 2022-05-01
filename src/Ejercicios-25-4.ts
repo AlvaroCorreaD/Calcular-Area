c4f2a89abd07f63d37beef1f1714bc0cf418d966
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-05-01 19:17:37 -0300

    Resolver

---
+++ src/Ejercicios-25-4.ts
@@ -0,0 +1,9 @@
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+let base: number = Number(dato1.value);
+let altura: number = Number(dato2.value);
+let area: number = base * altura;
+let btnEnv = document.getElementById("btnEnviararea");
+btnEnv.addEventListener("click", () => {
+  console.log("El area es:", area);
+});

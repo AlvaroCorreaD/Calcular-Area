f37a64dfa0b862c9cb8d662bcb285e754e638c86
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-05-09 17:33:56 -0300

    delete-comment

--- src/calcular-area-de-un-cuadrado.ts
+++ src/calcular-area-de-un-cuadrado.ts
@@ -1,14 +1,3 @@
-// let numero1 = document.getElementById("numero1");
-// let base: number = Number(numero1.value);
-// let numero2 = document.getElementById("numero2");
-// let altura: number = Number(numero2.value);
-// let area: number = Number(base * altura);
-// let btnEnv = document.getElementById("btnEnv");
-// rotulo.innerHTML = "Ingrese Base y Altura";
-// btnEnv.addEventListener("click", () => {
-//   console.log("El area es:", area, numero1.value * numero2.value);
-// });
-
 let btnEnv = document.getElementById("btnEnv");
 rotulo.innerHTML = "Ingrese Base y Altura";
 btnEnv.addEventListener("click", () => {
